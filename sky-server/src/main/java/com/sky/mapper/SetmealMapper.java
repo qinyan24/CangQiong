b491e42f9f0d48f0026cafec263b45f84d16e6a9
86186 <3481105372@qq.com>
2025-06-26 15:47:30 +0800

    商品浏览业务功能代码

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -6,15 +6,24 @@ import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishItemVO;
 import com.sky.vo.SetmealVO;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface SetmealMapper {
+
+    /**
+     * 修改套餐
+     * @param setmeal
+     */
+    @AutoFill(value = OperationType.UPDATE)
+    void update(Setmeal setmeal);
 
     /**
      * 根据分类id查询套餐的数量
@@ -51,9 +60,22 @@ public interface SetmealMapper {
     void deleteById(Long setmealId);
 
     /**
-     * 修改套餐
-     * @param setmeal
+     * 根据id查询套餐和套餐菜品关系
+     * @param id
+     * @return
      */
-    @AutoFill(value = OperationType.UPDATE)
-    void update(Setmeal setmeal);
+    SetmealVO getByIdWithDish(Long id);
+
+    /**
+     * 动态条件查询套餐
+     * @param setmeal
+     * @return
+     */
+    List<Setmeal> list(Setmeal setmeal);
+
+    @Select("select sd.name, sd.copies, d.image, d.description " +
+            "from setmeal_dish sd left join dish d on sd.dish_id = d.id " +
+            "where sd.setmeal_id = #{setmealId}")
+    List<DishItemVO> getDishItemBySetmealId(Long setmealId);
+
 }
